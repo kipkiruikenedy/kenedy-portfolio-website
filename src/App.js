b2bc3b79cc166659232d1163fb519726f66621f1
kipkiruikenedy <Kipkiruikenedy@gmail.com>
2022-04-13 22:15:06 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.scss';
+import { Navbar } from './components';
+import Header from './container/Header/Header';
+import About from './container/About/About';
+import Work from './container/Work/Work';
+import Skills from './container/Skills/Skills';
+import Testimonial from './container/Testimonial/Testimonial';
+import Footer from './container/Footer/Footer';
+
+
+const App = () => (
+  <div className="app">
+    <Navbar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer />
+  </div>
+);
+
+export default App;
